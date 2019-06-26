21fe5e75f39b969af956e99909e0ef0b9c6973f1
James John McGuire <jamesjohnmcguire@gmail.com>
2019-06-27 02:59:51 +0900

    Testing updates from a month ago.

---
+++ AndroidBindingsLibrary/Classes.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Com.Braintreepayments.Api.Dropin.Adapters
+{
+   // partial class VaultedPaymentMethodsAdapter
+    //{
+        //public override void OnBindViewHolder(RecyclerView.ViewHolder holder, int position)
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        //public override RecyclerView.ViewHolder OnCreateViewHolder(ViewGroup parent, int viewType)
+        //{
+        //    throw new NotImplementedException();
+        //}
+    //}
+
+
+
+}
+
+//namespace Naxam.BraintreeDropIn.Droid
+//{
+//    class Classes
+//    {
+//    }
+//}
